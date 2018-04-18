898b56bc14f7e871272ef5a63e92ac4fccfab4ea
Sunny Wang <Sunny.Wang@CapitalOne.com>
2018-04-18 13:03:45 -0400

    Add SPDX license headers.

--- RecorderDemo/RecorderDemo/ViewController.swift
+++ RecorderDemo/RecorderDemo/ViewController.swift
@@ -1,6 +1,8 @@
 /***********************************************************************************
 * Copyright 2015 Capital One Services, LLC
-
+* SPDX-License-Identifier: Apache-2.0
+* SPDX-Copyright: Copyright (c) Capital One Services, LLC
+ 
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
